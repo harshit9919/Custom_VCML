7222312cbe2da54b709657a6173f0ec3229cf057
harshit9919 <7007255214hg@gmail.com>
2025-06-18 14:41:00 +0530

    cmakelists_changes

--- build/systemc/src/CMakeFiles/systemc.dir/sysc/kernel/sc_cor_pthread.cpp.o.d
+++
@@ -1,3 +0,0 @@
-systemc/src/CMakeFiles/systemc.dir/sysc/kernel/sc_cor_pthread.cpp.o: \
- /home/kpit/Desktop/Task/VCML_Test/Custom_VCML/build/systemc-src/src/sysc/kernel/sc_cor_pthread.cpp \
- /usr/include/stdc-predef.h
